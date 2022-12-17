62460e0bbc00d0ee5e9448df67eb336f0867375d
akayi07 <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>
2022-12-17 10:44:07 +0800

    a

--- contracts/DummyVerifier.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-import "./Verifier.sol";
-contract DummyVerifier is DelphinusVerifier {
-  function verifyDelphinusTx(uint256[] calldata)
-    public pure override returns (bool) {
-    return true;
-  }
-}
--- contracts/Verifier.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface DelphinusVerifier {
-    /**
-     * @dev snark verification stub
-     */
-    function verifyDelphinusTx(uint256[] calldata args) external returns (bool);
-}
